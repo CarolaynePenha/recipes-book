79fc80f7d38d9a583f5dd61f0bc713a523b5c19e
Carolayne <carolaynepenha@gmail.com>
2024-07-26 11:35:14 -0300

    feat: see all information on the details page

--- app/routes/recipes/details.js
+++ app/routes/recipes/details.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class RecipesDetailsRoute extends Route {
+  @service store;
+  @service recipeData;
   async model(params) {
-    return 'Hello';
+    await this.recipeData.loadRecipes();
+    return this.store.peekRecord('recipe', params.recipe_id);
   }
 }
